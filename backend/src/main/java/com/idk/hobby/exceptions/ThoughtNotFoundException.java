ab31fdc030034508b2822ff0354113d5163c402c
Vesa Bence <vesabence@gmail.com>
2026-06-16 20:36:58 +0300

    added custom exception for ThoughtNotFound and global exception handler

---
+++ backend/src/main/java/com/idk/hobby/exceptions/ThoughtNotFoundException.java
@@ -0,0 +1,7 @@
+package com.idk.hobby.exceptions;
+
+public class ThoughtNotFoundException extends RuntimeException {
+    public ThoughtNotFoundException(String message) {
+        super(message);
+    }
+}
