2435e18376e5153ff96d16cc3572886ca944858a
Vesa Bence <vesabence@gmail.com>
2026-06-16 16:57:55 +0300

    created ThoughtService class

---
+++ src/main/java/services/ThoughtService.java
@@ -0,0 +1,12 @@
+package services;
+
+import lombok.AllArgsConstructor;
+import models.Thoughts;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class ThoughtService {
+    private Thoughts thoughts;
+
+}
