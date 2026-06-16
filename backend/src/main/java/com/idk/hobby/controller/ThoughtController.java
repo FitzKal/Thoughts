51b1e6f95ba4d4616f062a6c81bae857352567df
Vesa Bence <vesabence@gmail.com>
2026-06-16 21:21:54 +0300

    added CRUD endpoints to ThoughtController for managing thoughts

--- backend/src/main/java/com/idk/hobby/controller/ThoughtController.java
+++ backend/src/main/java/com/idk/hobby/controller/ThoughtController.java
@@ -1,10 +1,14 @@
 package com.idk.hobby.controller;
 
+import com.idk.hobby.interfaces.ThoughtMethods;
+import com.idk.hobby.models.ThoughtDTO;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import com.idk.hobby.services.ThoughtService;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
@@ -15,5 +19,30 @@ public class ThoughtController {
     @GetMapping
     public String hello(){
         return "Hello";
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ThoughtDTO> getThoughtById(@PathVariable Long id) {
+        return ResponseEntity.ok(thoughtService.getThoughtById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ThoughtDTO>> getAllThoughts() {
+        return ResponseEntity.ok(thoughtService.getAllThoughts());
+    }
+
+    @PostMapping
+    public ResponseEntity<ThoughtDTO> saveThought(@RequestBody ThoughtDTO thoughtDTO) {
+        return ResponseEntity.ok(thoughtService.saveThought(thoughtDTO));
+    }
+
+    @PutMapping
+    public ResponseEntity<ThoughtDTO> editThought(Long id, ThoughtDTO thoughtDTO) {
+        return ResponseEntity.ok(thoughtService.editThought(id,thoughtDTO));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<String> deleteThoughtById(Long id) {
+        return ResponseEntity.ok(thoughtService.deleteThoughtById(id));
     }
 }
