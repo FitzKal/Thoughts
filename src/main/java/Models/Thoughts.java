d77080978ddb7fb4e2838d70b3e9aa8f56e04c17
Vesa Bence <vesabence@gmail.com>
2026-06-16 16:47:13 +0300

    started implementing Thought model

---
+++ src/main/java/Models/Thoughts.java
@@ -0,0 +1,27 @@
+package Models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+public class Thoughts {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String name;
+    private LocalDate date;
+    private String description;
+    
+
+
+}
