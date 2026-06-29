b89745025c3fe867cac9d23c30f37316dd58ccc8
Vesa Bence <vesabence@gmail.com>
2026-06-29 17:39:59 +0300

    added ThoughtType with zod schema definition

---
+++ frontend/src/types/ThoughtType.ts
@@ -0,0 +1,9 @@
+import {z} from "zod";
+
+export const thoughtSchema = {
+    id:z.number(),
+    name:z.string("The name should be minimum 3 characters long").min(3),
+    description:z.string()
+}
+
+export type thoughtRequest = z.infer<typeof thoughtSchema>
