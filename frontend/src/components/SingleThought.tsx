1f35a76ae56f5cb0247b6863ea37fbc05d8f363d
Vesa Bence <vesabence@gmail.com>
2026-06-29 21:02:49 +0300

    updated SingleThought component to display date and description, added hover effect and adjusted CSS styles

--- frontend/src/components/SingleThought.tsx
+++ frontend/src/components/SingleThought.tsx
@@ -6,6 +6,8 @@ export default function SingleThought(){
             <h2 id={"FormTitle"}>Your added Thoughts</h2>
             <div className={"ThoughtDetails"}>
                 <p className={"detailText"}>Id: 1</p>
+                <p className={"detailText"}>Date: 20206.05.09</p>
+                <p className={"detailText"}>Description: sex</p>
             </div>
         </div>
     )
